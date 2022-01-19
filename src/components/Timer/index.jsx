83730d69b40357be2e92c12db69bd3e915bce344
riobits <23579928+RioBits@users.noreply.github.com>
2022-01-19 23:24:08 +0300

    add breaks

--- src/components/Timer/index.jsx
+++ src/components/Timer/index.jsx
@@ -5,12 +5,14 @@ import styles from './Timer.module.css'
 const Timer = () => {
   const [isRunning, setIsRunning] = useState(false)
   const [timeText, setTimeText] = useState('25:00')
-  const time = useRef(25 * 60)
+  const time = useRef(0.05 * 60)
+  const round = useRef(0)
 
   useEffect(() => {
     let timer
 
     if (isRunning) {
+      console.log(round.current)
       tick(time)
       timer = setInterval(() => {
         tick(time)
@@ -37,15 +39,63 @@ const Timer = () => {
 
     time.current = time.current - 1
 
-    // console.log('tick')
+    if (minutes === 0 && seconds === 0) {
+      const notificationSound = new Audio('./sounds/notification.mp3')
+      notificationSound.play()
+      new Notification("it's time to take break!", {
+        icon: './favicon.ico',
+        body: "Nice work! now le'ts take a break",
+      })
+
+      setIsRunning(false)
+
+      round.current = round.current + 1
+      if (round.current % 2 === 0) {
+        setTimeText('25:00')
+        time.current = 0.05 * 60
+      } else if (round.current % 7 === 0) {
+        setTimeText('15:00')
+        time.current = 0.05 * 60
+        round.current = -1
+      } else {
+        setTimeText('05:00')
+        time.current = 0.05 * 60
+      }
+    }
+    console.log('tick')
   }
 
   return (
     <div className={styles.container}>
       <h1>Pomodoro</h1>
       <p className={styles.time}>{timeText}</p>
-      <Button onClick={() => setIsRunning((prev) => !prev)}>
+      <Button
+        onClick={() => {
+          Notification.requestPermission()
+
+          setIsRunning((prev) => !prev)
+        }}
+      >
         {isRunning ? 'Pause' : 'Start'}
+      </Button>
+      <Button
+        onClick={() => {
+          setIsRunning(false)
+          round.current = round.current + 1
+          if (round.current % 2 === 0) {
+            setTimeText('25:00')
+            time.current = 0.05 * 60
+          } else if (round.current % 7 === 0) {
+            setTimeText('15:00')
+            time.current = 0.05 * 60
+            round.current = -1
+          } else {
+            setTimeText('05:00')
+            time.current = 0.05 * 60
+          }
+        }}
+      >
+        Skip
       </Button>
     </div>
   )
